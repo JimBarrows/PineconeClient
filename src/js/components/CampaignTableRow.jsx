6a49f524a70aa0c91bebbdf1dfe0d4fedec1bfd6
Jim Barrows <jim@reallybigtree.com>
2016-09-11 09:19:40 -0700

    Fixes https://trello.com/c/YK1JBXtL

--- src/js/components/CampaignTableRow.jsx
+++ src/js/components/CampaignTableRow.jsx
@@ -5,7 +5,7 @@ import RowControlButtons from "./controls/RowControlButtons";
 import {withRouter} from "react-router";
 
 
-export default withRouter(class CampaignTableRow extends React.Component {
+class CampaignTableRow extends React.Component {
 
 	constructor(props) {
 		super(props);
@@ -25,11 +25,14 @@ export default withRouter(class CampaignTableRow extends React.Component {
 	render() {
 		let {campaign} = this.props;
 		let {editing}  = this.state;
+		let dateFormat = "MMMM Do YYYY";
+		let effectiveFrom = campaign.effectiveFrom.format(dateFormat);
+		let effectiveThru = campaign.effectiveThru.isValid() ? campaign.effectiveThru.format(dateFormat) : "Ongoing";
 		return (
 				<tr>
 					<td>{campaign.name}</td>
-					<td>{campaign.effectiveFrom.format("MMMM Do YYYY")}</td>
-					<td>{campaign.effectiveThru.format("MMMM Do YYYY")}</td>
+					<td>{effectiveFrom}</td>
+					<td>{effectiveThru}</td>
 					<td>
 						<RowControlButtons id={campaign._id} editing={editing} edit={this.edit.bind(this)}
 						                   remove={this.remove.bind(this)}/>
@@ -43,4 +46,5 @@ export default withRouter(class CampaignTableRow extends React.Component {
 			editing: false
 		})
 	}
-});
+}
+export default withRouter(CampaignTableRow);
